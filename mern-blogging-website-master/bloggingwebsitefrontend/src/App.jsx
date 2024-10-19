0566b950ce39a44248b13b983cc827e2af943b20
lakmalprasanna <lakmalprasannakd@gmail.com>
2024-10-20 00:44:10 +0530

    Create route for Signin and signup un navbar and route between pages

--- mern-blogging-website-master/bloggingwebsitefrontend/src/App.jsx
+++ mern-blogging-website-master/bloggingwebsitefrontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <NavBar/>
+        <Routes>
+            <Route path="/" element={<NavBar />} >
+                <Route path="signin" element={<h1>Sigin in page</h1>} /> / +
+                <Route path="signup" element={<h1>Sigin up page</h1>} />
+            </Route>
+
+        </Routes>
     )
 }
 
