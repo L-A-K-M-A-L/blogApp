5ab5495c7e439ef8ba01e62c7c0e9c6628d2b5d4
lakmalprasanna <lakmalprasannakd@gmail.com>
2024-10-21 23:42:35 +0530

    Create session access to user when user login it will keep login if not user cna log in using credentials

--- mern-blogging-website-master/bloggingwebsitefrontend/src/App.jsx
+++ mern-blogging-website-master/bloggingwebsitefrontend/src/App.jsx
@@ -1,16 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext } from "react";
+import { useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
+
+
+//  token for user
+export const UserContext = createContext({});
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<NavBar />} >
-                <Route path="signin" element={<UserAuthForm type="sign-in"/>} />
-                <Route path="signup" element={<UserAuthForm type="sign-up"/>} />
-            </Route>
 
-        </Routes>
+    // state for user
+    const [userAuth, setUserAuth] = useState();
+
+
+    //  run once after rendering complete
+    useEffect(() => {
+        // getuser from the session
+        let userInSession = lookInSession("user");
+
+        userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
+
+    }, [])
+
+    return (
+        <UserContext.Provider value={{ userAuth, setUserAuth }}>
+            <Routes>
+                <Route path="/" element={<NavBar />} >
+                    <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+                    <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+                </Route>
+
+            </Routes>
+        </UserContext.Provider>
+
     )
 }
 
--- mern-blogging-website-master/bloggingwebsitefrontend/src/pages/userAuthForm.page.jsx
+++ mern-blogging-website-master/bloggingwebsitefrontend/src/pages/userAuthForm.page.jsx
@@ -1,23 +1,31 @@
 import googleIcon from "../imgs/google.png";
 import InputBox from "../components/input.component";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import { Toaster, toast } from "react-hot-toast"
 import axios from "axios";
 import { storeInSession } from "../common/session";
+import { useContext, useRef } from "react";
+import { UserContext } from "../App";
 
 
 
 
 const UserAuthForm = ({ type }) => {
 
+    const authForm = useRef();
 
-    const userAuthThroughServer = ( serverRoute, formData ) => {
+    // import user-details, import from app.jsx
+    let { userAuth, setUserAuth } = useContext(UserContext);
+    let access_token = userAuth ? userAuth.access_token : null;
+    console.log(access_token);
+
+    const userAuthThroughServer = (serverRoute, formData) => {
 
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
             .then(({ data }) => {
                 storeInSession("user", JSON.stringify(data))
-                console.log(sessionStorage)
+                setUserAuth(data)
 
             })
             .catch(({ response }) => {
@@ -29,7 +37,7 @@ const UserAuthForm = ({ type }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        let serverRoute = type == "sign-in" ? "/signin" : "/signup" ;
+        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
 
         let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
         let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
@@ -70,78 +78,81 @@ const UserAuthForm = ({ type }) => {
     }
 
     return (
-        <AnimationWrapper keyValue={type}>
-            <section className="h-cover flex items-center justify-center">
-                <Toaster />
-                <form id="formElement" className="w-[80%] max-w-[400px]">
-                    <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                        {type == "sign-in" ? "Welcome Back" : "Join Us Today"}
-                    </h1>
+        access_token ?
+            // this will go back to home page if user session expired
+            <Navigate to="/"/> :
+            <AnimationWrapper keyValue={type}>
+                <section className="h-cover flex items-center justify-center">
+                    <Toaster />
+                    <form id="formElement" className="w-[80%] max-w-[400px]">
+                        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                            {type == "sign-in" ? "Welcome Back" : "Join Us Today"}
+                        </h1>
 
-                    {
-                        type != "sign-in" ?
-                            <InputBox
-                                name="fullname"
-                                type="text"
-                                placeholder="Full Name"
-                                icon="fi-rr-user"
-                            /> : ""
-                    }
+                        {
+                            type != "sign-in" ?
+                                <InputBox
+                                    name="fullname"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    icon="fi-rr-user"
+                                /> : ""
+                        }
 
-                    <InputBox
-                        name="email"
-                        type="email"
-                        placeholder="Email"
-                        icon="fi-rr-envelope"
-                    />
+                        <InputBox
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                            icon="fi-rr-envelope"
+                        />
 
-                    <InputBox
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        icon="fi-rr-key"
-                    />
+                        <InputBox
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            icon="fi-rr-key"
+                        />
 
-                    <button
-                        className="btn-dark center mt-14"
-                        type="submit"
-                        onClick={handleSubmit}
-                    >
-                        {type.replace('-', " ")}
-                    </button>
+                        <button
+                            className="btn-dark center mt-14"
+                            type="submit"
+                            onClick={handleSubmit}
+                        >
+                            {type.replace('-', " ")}
+                        </button>
 
-                    <div
-                        className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold"
-                    >
-                        <hr className="w-1/2 border-black" />
-                        <p>or</p>
-                        <hr className="w-1/2 border-black" />
+                        <div
+                            className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold"
+                        >
+                            <hr className="w-1/2 border-black" />
+                            <p>or</p>
+                            <hr className="w-1/2 border-black" />
 
-                    </div>
-                    <button className="btn-dark flex items-center justify-center gap-4 nw-[90%] center">
-                        <img src={googleIcon} alt="googleIcon" className="w-5 " />
-                        Continue with google
-                    </button>
+                        </div>
+                        <button className="btn-dark flex items-center justify-center gap-4 nw-[90%] center">
+                            <img src={googleIcon} alt="googleIcon" className="w-5 " />
+                            Continue with google
+                        </button>
 
-                    {
-                        type == "sign-in" ?
-                            <p className="mt-6 text-dark-grey text-xl text-center"> Don't Have an Account ?
-                                <Link to="/signup" className="underline text-black text-xl ml-1">
-                                    Join Us today.
-                                </Link>
-                            </p> :
-                            <p className="mt-6 text-dark-grey text-xl text-center"> Already member ?
-                                <Link to="/signin" className="underline text-black text-xl ml-1">
-                                    Sign in here
-                                </Link>
-                            </p>
-                    }
+                        {
+                            type == "sign-in" ?
+                                <p className="mt-6 text-dark-grey text-xl text-center"> Don't Have an Account ?
+                                    <Link to="/signup" className="underline text-black text-xl ml-1">
+                                        Join Us today.
+                                    </Link>
+                                </p> :
+                                <p className="mt-6 text-dark-grey text-xl text-center"> Already member ?
+                                    <Link to="/signin" className="underline text-black text-xl ml-1">
+                                        Sign in here
+                                    </Link>
+                                </p>
+                        }
 
-                </form>
+                    </form>
 
 
-            </section>
-        </AnimationWrapper>
+                </section>
+            </AnimationWrapper>
 
     )
 
